c82a7ded10c7757d77d434f9f17d81d3287d482d
Joeb <Joeb@JOEB-DESK17>
2017-07-05 23:16:47 -0500

    Add project files.

---
+++ BatteryDischarger/BatteryDischarger.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BatteryDischarger.ino
+ Created:	7/5/2017 11:16:43 PM
+ Author:	Joeb
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
